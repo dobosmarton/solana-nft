b997b59dc7bc571ac3d73ceb195f5c2beaa56137
Marton Dobos <dobeis2@gmail.com>
2021-12-05 22:11:13 +0100

    chore: project setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    opacity: ({ after }) => after(['disabled']),
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
